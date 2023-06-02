1350582f081fdcbf18f4f66a933149ce14631c11
Emily Martinez <emilymdj08@gmail.com>
2023-06-02 16:08:00 -0700

    replacing with real files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("Your index.js file is loaded correctly!");
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('mouseenter', () => {
+    button.style.backgroundColor = '#3da000';
+    button.style.color = '#FFFFFF';
+  });
+  button.addEventListener('mouseleave', () => {
+    button.style.backgroundColor = '#373738';
+    button.style.color = '#FFFFFF';
+  });
+});
+
